9103615b03ec03fe81c05f18adf30e60bbcc6aaf
Milena Grncarova <milenagrncarov@outlook.com>
2021-04-04 18:09:39 +0200

    Implementation of segment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserHistory } from "history";
+
+
+
+const history = createBrowserHistory();
+
+let prevPath = null;
+
+// listen and notify Segment of client-side page updates
+history.listen((location) => {
+  if (location.pathname !== prevPath) {
+    prevPath = location.pathname;
+  }
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
